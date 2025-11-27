cc9b016cfd260eead6f56e5bbba167a5bd4e0dd6
AxelCarrillo <122809603+AxelCarrillo@users.noreply.github.com>
2025-11-26 19:35:34 -0600

    Refactor inicio.php for improved UI and features
    Updated the HTML structure and improved the user interface elements. Added new features for managing donors and enhanced the visual effects.

--- inicio.php
+++ inicio.php
@@ -18,8 +18,9 @@ $rol_usuario = isset($_SESSION['rol']) ? $_SESSION['rol'] : '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="/favicon.ico">
     <title>Sistema Presidencial de Apoyo Alimentario</title>
+    <link rel="icon" type="image/png" href="favicon.png">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/style_inicio.css">
 </head>
@@ -179,4 +180,5 @@ $rol_usuario = isset($_SESSION['rol']) ? $_SESSION['rol'] : '';
 </body>
 
 </html>
+
 
