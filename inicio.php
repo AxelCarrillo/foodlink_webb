32fca47f150735cbd64e4fb2f828b22173311638
AxelCarrillo <122809603+AxelCarrillo@users.noreply.github.com>
2025-11-26 19:33:16 -0600

    Enhance inicio.php with donor viewing section
    Added a new section for viewing donors and improved the layout.

--- inicio.php
+++ inicio.php
@@ -18,6 +18,7 @@ $rol_usuario = isset($_SESSION['rol']) ? $_SESSION['rol'] : '';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
     <title>Sistema Presidencial de Apoyo Alimentario</title>
     <link rel="stylesheet" href="css/navbar.css">
     <link rel="stylesheet" href="css/style_inicio.css">
@@ -176,4 +177,5 @@ $rol_usuario = isset($_SESSION['rol']) ? $_SESSION['rol'] : '';
         document.head.appendChild(style);
     </script>
 </body>
+
 </html>
